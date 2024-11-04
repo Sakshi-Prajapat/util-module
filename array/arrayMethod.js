81cd0dda4adb4c0aa99d18415d88cc1613037dfb
Sakshi Prajapat <sakshiprajapat@thoughtwin.com>
2024-11-04 19:15:30 +0530

    methods

--- array/arrayMethod.js
+++ array/arrayMethod.js
@@ -34,9 +34,9 @@ const closeFlatBtn = document.querySelector(".close-flat-modal-btn");
 const closeIndexOfBtn = document.querySelector(".close-indexOF-modal-btn");
 
 
-// function openModal() {
-//     modal.classList.remove("hide");
-// }
+function getElemtntId(id) {
+    return document.getElementById(id);
+}
 
 function closeFindModal(e, clickedOutside) {
     if (clickedOutside) {
@@ -108,42 +108,144 @@ function closeIndexOfModal(e, clickedOutside) {
     } else indexOfModal.classList.add("hide");
 }
 
-openFindButton.addEventListener("click", ()=>{ findModal.classList.remove("hide");});
+openFindButton.addEventListener("click", () => { findModal.classList.remove("hide"); });
 findModal.addEventListener("click", (e) => closeFindModal(e, true));
 closeBtn.addEventListener("click", closeFindModal);
 
-openMapButton.addEventListener("click", ()=>{ mapModal.classList.remove("hide");});
+openMapButton.addEventListener("click", () => { mapModal.classList.remove("hide"); });
 mapModal.addEventListener("click", (e) => closeMapModal(e, true));
 closeMapBtn.addEventListener("click", closeMapModal);
 
-openFilterButton.addEventListener("click", ()=>{ filterModal.classList.remove("hide");});
+openFilterButton.addEventListener("click", () => { filterModal.classList.remove("hide"); });
 filterModal.addEventListener("click", (e) => closeFilterModal(e, true));
 closeFilterBtn.addEventListener("click", closeFilterModal);
 
-openSortButton.addEventListener("click", ()=>{ sortModal.classList.remove("hide");});
+openSortButton.addEventListener("click", () => { sortModal.classList.remove("hide"); });
 sortModal.addEventListener("click", (e) => closeSortModal(e, true));
 closeSortBtn.addEventListener("click", closeSortModal);
 
-openReduceButton.addEventListener("click", ()=>{ reduceModal.classList.remove("hide");});
+openReduceButton.addEventListener("click", () => { reduceModal.classList.remove("hide"); });
 reduceModal.addEventListener("click", (e) => closeReduceModal(e, true));
 closeReduceBtn.addEventListener("click", closeReduceModal);
 
-openSliceButton.addEventListener("click", ()=>{ sliceModal.classList.remove("hide");});
+openSliceButton.addEventListener("click", () => { sliceModal.classList.remove("hide"); });
 sliceModal.addEventListener("click", (e) => closeSliceModal(e, true));
 closeSliceBtn.addEventListener("click", closeSliceModal);
 
-openSpliceButton.addEventListener("click", ()=>{ spliceModal.classList.remove("hide");});
+openSpliceButton.addEventListener("click", () => { spliceModal.classList.remove("hide"); });
 spliceModal.addEventListener("click", (e) => closeSpliceModal(e, true));
 closeSpliceBtn.addEventListener("click", closeSpliceModal);
 
-openEnteriesButton.addEventListener("click", ()=>{ enteriesModal.classList.remove("hide");});
+openEnteriesButton.addEventListener("click", () => { enteriesModal.classList.remove("hide"); });
 enteriesModal.addEventListener("click", (e) => closeEnteriesModal(e, true));
 closeEnteriesBtn.addEventListener("click", closeEnteriesModal);
 
-openFlatButton.addEventListener("click", ()=>{ flatModal.classList.remove("hide");});
+openFlatButton.addEventListener("click", () => { flatModal.classList.remove("hide"); });
 flatModal.addEventListener("click", (e) => closeFlatModal(e, true));
 closeFlatBtn.addEventListener("click", closeFlatModal);
 
-openIndexOFButton.addEventListener("click", ()=>{ indexOfModal.classList.remove("hide");});
+openIndexOFButton.addEventListener("click", () => { indexOfModal.classList.remove("hide"); });
 indexOfModal.addEventListener("click", (e) => closeIndexOfModal(e, true));
 closeIndexOfBtn.addEventListener("click", closeIndexOfModal);
+
+
+function findMethod(event) {
+    event.preventDefault()
+    const findId = getElemtntId("findElement")
+    const number = getElemtntId("number")
+    const resultDiv = getElemtntId("resultDiv")
+    let element = findId.value.split(",")
+    console.log(number.value)
+    const result = element.find((item) => {
+        return item > number.value
+    })
+    resultDiv.textContent = "The Result is :- " + result
+    resultDiv.style.fontSize = 30
+   
+    findId.value = " "
+}
+function filterMethod(event) {
+    event.preventDefault()
+    const filterId = getElemtntId("filterElement")
+    const resultDiv = getElemtntId("filterResultDiv")
+    let element = filterId.value.split(",")
+    console.log(element)
+    const result = element.filter((item) => {
+        return item % 2 == 0;
+    })
+    resultDiv.textContent = "The Result is :- " + result
+    resultDiv.style.fontSize = 30
+
+    filterId.value = " "
+    
+   
+}
+
+function sortMethod(event) {
+    event.preventDefault();
+    const sortTypeOne = getElemtntId("ascending")
+    const sortTypeTwo = getElemtntId("descending")
+    
+    const sortId = getElemtntId("sortElement")
+    const resultDiv = getElemtntId("sortResultDiv")
+    let element = sortId.value.split(",")
+    // console.log(sortTypeOne.checked.value , sortTypeTwo.checked.value )
+    if (sortTypeOne.checked) {
+        element.sort((a, b) => a - b);
+        resultDiv.textContent = "The Result is :- " + element
+        resultDiv.style.fontSize = 30
+    } else if (sortTypeTwo.checked) {
+        element.sort((a, b) => b - a);
+        resultDiv.textContent = "The Result is :- " + element
+        resultDiv.style.fontSize = 30
+    }
+    sortId.value=" "
+  }
+
+  function reduceMethod(event){
+    event.preventDefault()
+    const reduceId = getElemtntId("reduceElement")
+    const resultDiv = getElemtntId("reduceResultDiv")
+    let element = reduceId.value.split(",")
+  }
+
+  function sliceMethod(event){
+    event.preventDefault()
+    const sliceId = getElemtntId("sliceElement")
+    const resultDiv = getElemtntId("sliceResultDiv")
+    let element = sliceId.value.split(",")
+  }
+
+  function spliceMethod(event){
+    event.preventDefault()
+    const spliceId = getElemtntId("spliceElement")
+    const resultDiv = getElemtntId("spliceResultDiv")
+    let element = spliceId.value.split(",")
+  }
+
+  function enteriesMethod(event){
+    event.preventDefault()
+    const enteriesId = getElemtntId("enteriesElement")
+    const resultDiv = getElemtntId("enteriesResultDiv")
+    let element = spliceId.value.split(",")
+  }
+function findInputCheck(event) {
+    event.preventDefault()
+    const arrayRegex = /^[-,0-9]+$/
+    const element = getElemtntId("findElement")
+    const findError = getElemtntId("findError")
+    if (!element.value) {
+        findError.textContent = "Element is required";
+        element.style.border = "2px solid red";
+        // isNameValid = false;
+    } else if (!arrayRegex.test(element.value)) {
+        findError.textContent = "Enter valid name ";
+        element.style.border = "2px solid red";
+        // isNameValid = false;
+    } else {
+        findError.textContent = "";
+        element.style.border = "2px solid green";
+        isNameValid = true;
+    }
+}
+
